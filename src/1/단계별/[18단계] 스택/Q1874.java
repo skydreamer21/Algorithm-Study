792e58003248309799b17f6e363179b91ec49c33
skydreamer <skydreamer210@gmail.com>
2022-02-03 15:03:33 +0900

    22.02.03 pm 3:03

--- src/1/단계별/[18단계] 스택/Q1874.java
+++ src/1/단계별/[18단계] 스택/Q1874.java
@@ -1,10 +1,9 @@
-// 1874번 스택 수열
-
+// 17298번 오큰수
 /*
 <문제 정보>
- 1. 스택을 이용해 입력된 수열을 만들기 위해 필요한 연산을 한줄에 한개씩 출력
- 2. push : + / pop : -
- 3. 1~n 에서 n은 100,000이하 자연수
+ 1. 크기가 N인 수열에서 i번째 수에 대한 오큰수는 오른쪽에 있으면서 첫번째로 큰수
+ 2. N개의 수에 대한 오큰수 NGE(N)을 출력
+ 3. 1<=N<=1,000,000
 
 <프로그램 진행>
  1.
@@ -17,41 +16,30 @@
 
 import Necessary_Class.MyStack;
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Q1874 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
         MyStack<Integer> stack = new MyStack<>(N);
-        int prev_num;
-        int num=0;
         int max=0;
-        boolean possible = true;
-        while(N-->0) {
-            prev_num = num;
-            num = Integer.parseInt(br.readLine());
-            if (num < prev_num) {
-                if (stack.pop()!=num) {
-                    possible=false;
-                    break;
-                }
-                else sb.append("-\n");
+        for (int i=0;i<N;i++) arr[i] = Integer.parseInt(st.nextToken());
+        for (int i=N-1;i>=0;i--) {
+            if (arr[i]>max || stack.empty()) {
+                stack.clear();
+                max = arr[i];
+                stack.push(arr[i]);
+                arr[i]=-1;
             }
-            else if (num > prev_num) {
-                int i = num-prev_num;
-                while (i-->0) {
-                    stack.push(++max);
-                    sb.append("+\n");
-                }
-                stack.pop();
-                sb.append("-\n");
-            }
-
-
+            else if (arr[i]<max) arr[i] = max;
+            else if (arr[i]==max) arr[i]=-1;
         }
-
+        for (int i=0;i<N;i++) sb.append(arr[i]).append(" ");
         bw.write(sb.toString());
         bw.flush();
         bw.close();
