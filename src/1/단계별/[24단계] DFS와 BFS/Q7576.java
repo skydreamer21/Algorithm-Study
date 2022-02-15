8ab194bec9df2e4c3d0b4091d1897212994bb3f5
skydreamer <skydreamer210@gmail.com>
2022-02-15 14:22:35 +0900

    22.02.15 pm2:22

---
+++ src/1/단계별/[24단계] DFS와 BFS/Q7576.java
@@ -0,0 +1,35 @@
+// 7576번 토마토
+/*
+<문제 정보>
+ 1.
+
+<변수 범위>
+ 1.
+
+<프로그램 진행>
+ 1. 1초 / 256MB
+ 2.
+
+<필요 함수>
+ 1.
+
+ */
+
+
+import java.io.*;
+
+public class Q7576 {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
