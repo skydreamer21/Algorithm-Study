dc54175d208ccbad713f030e7f11a8282ec2f94c
skydreamer21 <skydreamer210@gmail.com>
2022-09-04 22:29:08 +0900

    [Programmers][1] 모음사전 by JS

---
+++ src/Programmers/level2/모음_사전.js
@@ -0,0 +1,55 @@
+function solution(word) {
+    const NUM_OF_CHAR = 5;
+    const EMPTY = 0;
+    const map = new Map();
+    map.set('A', 1);
+    map.set('E', 2);
+    map.set('I', 3);
+    map.set('O', 4);
+    map.set('U', 5);
+
+    const dp = new Array(6).fill(0);
+    const len = word.length;
+    let numOfCases = 0;
+    for (let i=1;i<=len;i++) {
+        const charOrderInDict = map.get(word[i-1]);
+        const emptySpace = NUM_OF_CHAR - i;
+        numOfCases++;
+        numOfCases += (charOrderInDict-1)*eachDigitNumOfCases(emptySpace);
+        // console.log(`charOrderInDict:${charOrderInDict}, emptySpace:${emptySpace}`);
+        // console.log(`i:${i}, numOfCases:${numOfCases}`);
+    }
+
+    return numOfCases;
+
+
+    // n=0 빈자리가 0
+    // n=1 빈자리가 1
+
+    function eachDigitNumOfCases(n) {
+        if (n==0) {
+            return dp[n] = 1;
+        }
+        if (dp[n] != EMPTY) return dp[n];
+
+        dp[n] = 1 + NUM_OF_CHAR*(eachDigitNumOfCases(n-1));
+        return dp[n];
+    }
+}
+
+/*
+const VOWELS = ['A','E','I','O','U','']
+
+const f=(a)=>VOWELS.map(b=>b+a)
+
+function solution(word) {
+    const ans = Array.from(
+        new Set(VOWELS
+            .map(f).flat()
+            .map(f).flat()
+            .map(f).flat()
+            .map(f).flat())
+        ).sort().indexOf(word)
+    return ans;
+}
+*/
